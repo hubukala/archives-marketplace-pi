cd3fe17a31893e59b2e6eca904fc50ca663bcf17
hubukala <hubertbukala@icloud.com>
2024-05-11 12:53:22 +0200

    added types for products

---
+++ types/Product.ts
@@ -0,0 +1,11 @@
+export interface ProductType {
+    id: string
+    image: string
+    title: string
+    size: string
+    price: string | number
+    category: string
+    condition: string
+    description: string
+    designer: string
+}
