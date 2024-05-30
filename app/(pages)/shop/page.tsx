0ed0a2e1d8e7281a0e4ff8ce8d953f7c3c46a04f
hubukala <hubertbukala@icloud.com>
2024-05-30 22:00:41 +0200

    products api added

--- app/(pages)/shop/page.tsx
+++ app/(pages)/shop/page.tsx
@@ -1,46 +1,26 @@
-'use client'
-import { collection, getDocs, query, where } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { db } from "@/lib/firebase/firebase-config";
+'use client';
+import useSWR from 'swr';
+import axios from 'axios';
 import FilterArray from "@/app/utils/filter-array";
 import { ProductsContainer } from "./_components/style";
 import ProductsList from "@/app/components/ui/products-list/products-list";
 import { ProductType } from "@/types/Product";
 
-export default function All() {
-    const [data, setData] = useState<any>([]);
+const fetcher = (url: string) => axios.get(url).then(res => res.data);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const q = query(
-                collection(db, 'products'),
-                where('available', '==', true),
-            );
-            const querySnapshot = await getDocs(q);
-            const newData: ProductType[] = [];
-            querySnapshot.forEach((doc) => {
-                newData.push({
-                    category: doc.data().category,
-                    condition: doc.data().condition,
-                    description: doc.data().description,
-                    designer: doc.data().designer,
-                    image: doc.data().images[0],
-                    price: doc.data().price,
-                    id: doc.data().product_id,
-                    size: doc.data().size,
-                    title: doc.data().title,
-                });
-            });
-            setData(newData);
-        };
-        fetchData()
-    }, [])
+const All: React.FC = () => {
+  const { data, error, isLoading } = useSWR('/api/products', fetcher);
 
-    const FilteredProducts = FilterArray({cat: 'All', arr: data as ProductType[]});
+  if (error) return <div>Failed to load products</div>;
+  if (isLoading) return <div>Loading...</div>
 
-    return (
-        <ProductsContainer>
-            <ProductsList arr={FilteredProducts as ProductType[]}/>
-        </ProductsContainer>
-    )
-}
+  const FilteredProducts = FilterArray({ cat: 'All', arr: data?.products });
+
+  return (
+    <ProductsContainer>
+      <ProductsList arr={FilteredProducts as ProductType[]}/>
+    </ProductsContainer>
+  );
+};
+
+export default All;
