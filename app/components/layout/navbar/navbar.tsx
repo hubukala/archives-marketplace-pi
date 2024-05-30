4c6e7caaca6c20b2f2f623cc5258bd9889e2b265
hubukala <hubertbukala@icloud.com>
2024-05-30 21:57:42 +0200

    layout changes

--- app/components/layout/navbar/navbar.tsx
+++ app/components/layout/navbar/navbar.tsx
@@ -5,8 +5,24 @@ import { NavBarWrapper } from "./style"
 import { ButtonsContainer } from "../../ui/button/style"
 import Button from "../../ui/button/button"
 
+type NavBarProps = {
+    setLoginModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+    setRegisterModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
 
-const NavBar = () => {
+type ModalTypes = 'login' | 'register'
+
+const NavBar = ({setLoginModalOpen, setRegisterModalOpen}: NavBarProps) => {
+    const handleOpenModal = (modalType: ModalTypes) => {
+        if (modalType === 'login') {
+            setRegisterModalOpen(false)
+            setLoginModalOpen(true)
+        } else {
+            setLoginModalOpen(false)
+            setRegisterModalOpen(true)
+        }
+    }
+
     return (
         <NavBarWrapper>
             <S.NavLink href="/">ARCHIVES MARKETPLACE</S.NavLink>
@@ -14,8 +30,8 @@ const NavBar = () => {
             <S.NavLink href="/sell">SELL</S.NavLink>
             <S.NavLink href="/shop">SHOP</S.NavLink>
             <ButtonsContainer>
-                <Button label="LOGIN" variant="primary"/>
-                <Button label="SIGN UP" variant="secondary"/>
+                <Button label="LOGIN" variant="primary" onClick={() => handleOpenModal('login')}/>
+                <Button label="SIGN UP" variant="secondary" onClick={() => handleOpenModal('register')}/>
             </ButtonsContainer>
         </NavBarWrapper>
     )
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@
 import "./globals.css";
 import StyledComponentsRegistry from "@/lib/styled-components/registry";
 import NavBar from "./components/layout/navbar/navbar";
+import Modal from "./components/ui/modal/modal";
+import { useState } from "react";
+import LoginForm from "./components/layout/navbar/_components/login-form/login-form";
+import RegisterForm from "./components/layout/navbar/_components/register-form/register-form";
 
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -16,14 +20,26 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [loginModalOpen, setLoginModalOpen] = useState(false)
+  const [registerModalOpen, setRegisterModalOpen] = useState(false)
+
   return (
     <html lang="en">
       <body>        
         <StyledComponentsRegistry>
-          <NavBar />
+          <NavBar
+            setLoginModalOpen={setLoginModalOpen}
+            setRegisterModalOpen={setRegisterModalOpen}
+          />
           <main>
             {children}
           </main>
+          <Modal open={loginModalOpen}>
+            <LoginForm setLoginModalOpen={setLoginModalOpen}/>
+          </Modal>
+          <Modal open={registerModalOpen}>
+            <RegisterForm setRegisterModalOpen={setRegisterModalOpen}/>
+          </Modal>
         </StyledComponentsRegistry>
       </body>
     </html>
