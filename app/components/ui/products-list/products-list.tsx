c903d82f22f69d9ea9db1b33d939036de4da9993
hubukala <hubertbukala@icloud.com>
2024-06-29 15:29:37 +0200

    products list component updated

--- app/components/ui/products-list/products-list.tsx
+++ app/components/ui/products-list/products-list.tsx
@@ -2,21 +2,30 @@ import ProductCard from "../product-card/product-card";
 import { ProductType } from "@/types/Product";
 
 type ProductsListProps = {
-    arr: Array<ProductType>
-}
+    arr: Array<ProductType>;
+    message?: string;
+};
 
-const ProductsList = ({arr}: ProductsListProps) => {
-    return arr?.map((item: ProductType) => (
-        <ProductCard
-            key={item.id}
-            image={item.image}
-            title={item.title}
-            size={item.size}
-            price={item.price}
-            id={item.id}
-            category={item.category}
-        />
-    ));
-}
+const ProductsList = ({ arr, message }: ProductsListProps) => {
+    return (
+        <>
+            {arr.length < 1 ? (
+                <p>{message && message}</p>
+            ) : (
+                arr?.map((item: ProductType) => (
+                    <ProductCard
+                        key={item.id}
+                        image={item.image}
+                        title={item.title}
+                        size={item.size}
+                        price={item.price}
+                        id={item.id}
+                        category={item.category}
+                    />
+                ))
+            )}
+        </>
+    );
+};
 
-export default ProductsList
+export default ProductsList;
