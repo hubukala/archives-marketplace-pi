b3ab9c28756c641ed2e129d7f316b2846e2cdb90
hubukala <hubertbukala@icloud.com>
2024-06-29 15:29:07 +0200

    added listings and orders pages

--- app/(pages)/account/orders/page.tsx
+++ app/(pages)/account/orders/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+import { useUserOrders } from "@/lib/api/user-orders";
+import { ProductsContainer } from "../../shop/_components/style";
+import ProductsList from "@/app/components/ui/products-list/products-list";
+import { ProductType } from "@/types/Product";
+
 export default function Orders() {
+    const { listings, isLoading, isError } = useUserOrders();
+
+    if (isLoading) return <div>Loading...</div>;
+    if (isError) return <div>Error loading listings</div>;
+
     return (
-        <div>
-            orders
-        </div>
-    )
+        <ProductsContainer>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <ProductsList
+                    arr={listings as ProductType[]}
+                    message="No orders found"
+                />
+            )}
+            {isError && <p>Failed to fetch products</p>}
+        </ProductsContainer>
+    );
 }
--- app/(pages)/account/posted-items/page.tsx
+++ app/(pages)/account/posted-items/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { useUserListings } from "@/lib/api/user-listings";
+import { ProductsContainer } from "../../shop/_components/style";
+import ProductsList from "@/app/components/ui/products-list/products-list";
+import { ProductType } from "@/types/Product";
+
 export default function PostedItems() {
+    const { listings, isLoading, isError } = useUserListings();
+
+    if (isLoading) return <div>Loading...</div>;
+    if (isError) return <div>Error loading listings</div>;
+
     return (
-        <div>
-            my posted items
-        </div>
-    )
+        <ProductsContainer>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <ProductsList arr={listings as ProductType[]} />
+            )}
+            {isError && <p>Failed to fetch products</p>}
+        </ProductsContainer>
+    );
 }
