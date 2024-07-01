794055d78200eedd06f1ed7f3c4db2fb3783df3e
hubukala <hubertbukala@icloud.com>
2024-07-01 21:17:24 +0200

    added api for posting new products

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,7 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+    collection,
+    doc,
+    getDocs,
+    query,
+    setDoc,
+    where,
+} from "firebase/firestore";
 import { db } from "@/lib/firebase/config";
 import { ProductType } from "@/types/Product";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getAuth } from "firebase-admin/auth";
+import { v4 as uuidv4 } from "uuid";
 
 export async function GET(req: NextRequest) {
     const { searchParams } = new URL(req.url);
@@ -32,6 +42,66 @@ export async function GET(req: NextRequest) {
         });
         return NextResponse.json({ products });
     } catch (err) {
+        return NextResponse.json(
+            { error: "Failed to fetch products", err },
+            { status: 500 }
+        );
+    }
+}
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+    const auth = getAuth();
+    const token = req?.headers.get("authorization").split(" ")[1];
+    if (!token) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const uniqueId = uuidv4();
+
+    try {
+        const decodedToken = await auth.verifyIdToken(token);
+        const uid = decodedToken.uid;
+
+        const {
+            itemTitle,
+            description,
+            size,
+            color,
+            designer,
+            category,
+            condition,
+            price,
+            images,
+            shippingCarrier,
+            shippingPrice,
+        } = await req.json();
+
+        // const formData = await req.json();
+        // console.log(formData);
+
+        await setDoc(doc(db, "products", uniqueId), {
+            user_id: uid,
+            product_id: uniqueId,
+            available: true,
+            title: itemTitle.toLocaleUpperCase(),
+            description,
+            size,
+            color,
+            designer,
+            category,
+            condition,
+            price: parseFloat(price),
+            images,
+            shippingCarrier,
+            shippingPrice: parseFloat(shippingPrice),
+        });
+
+        return NextResponse.json(
+            { message: "Product uploaded successfully" },
+            { status: 200 }
+        );
+    } catch (err) {
+        console.log("catch", err);
         return NextResponse.json(
             { error: "Failed to fetch products", err },
             { status: 500 }
