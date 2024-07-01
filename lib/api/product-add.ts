bc10fdefd3b233ee73373f0a6c14c66de1b9faa7
hubukala <hubertbukala@icloud.com>
2024-07-01 21:27:14 +0200

    added hook for posting products

---
+++ lib/api/product-add.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState } from "react";
+import getValidToken from "@/app/utils/get-valid-token";
+
+const useProductAdd = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const productAdd = async (productData: string) => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const token = await getValidToken();
+            const response = await axios.post("/api/products", productData, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            });
+
+            setLoading(false);
+            return response.data;
+        } catch (err) {
+            console.error("Upload failed:", err);
+            setError("Failed to upload product");
+            setLoading(false);
+        }
+    };
+
+    return { productAdd, loading, error };
+};
+
+export default useProductAdd;
