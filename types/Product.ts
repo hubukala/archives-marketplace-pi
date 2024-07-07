b9dc48ad9e7ca40096cb0db854b73d9e647a70c4
hubukala <hubertbukala@icloud.com>
2024-07-07 09:55:35 +0200

    types fixed

--- types/Product.ts
+++ types/Product.ts
@@ -1,11 +1,14 @@
 export interface ProductType {
-    id: string
-    image: string
-    title: string
-    size: string
-    price: string | number
-    category: string
-    condition: string
-    description: string
-    designer: string
+    id: string;
+    image: string[];
+    title: string;
+    size: string;
+    price: string | number;
+    category: string;
+    condition: string;
+    description: string;
+    designer: string;
+    user_id: string;
+    buyer_id?: string;
+    available?: boolean;
 }
