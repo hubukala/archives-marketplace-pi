b148312c75206db4432b672f227cb5b1a7e6799f
hubukala <hubertbukala@icloud.com>
2024-07-17 21:56:55 +0200

    purchase api changed

--- app/api/product-purchase/route.ts
+++ app/api/product-purchase/route.ts
@@ -1,3 +1,4 @@
+import { sendEmail } from "@/app/utils/send-email";
 import { db } from "@/lib/firebase/config";
 import { getAuth } from "firebase-admin/auth";
 import { doc, setDoc } from "firebase/firestore";
@@ -17,21 +18,47 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
 
         const { buyerId, purchaseDate, productId } = await req.json();
 
-        if (uid) {
-            await setDoc(
-                doc(db, "products", productId),
-                {
-                    buyer_id: buyerId,
-                    purchase_date: purchaseDate,
-                    available: false,
-                },
-                {
-                    merge: true,
-                }
-            );
-        } else {
-            return NextResponse.json({ error: "Unathorized" }, { status: 401 });
-        }
+        // disabled for testing email notifications
+        // if (uid) {
+        //     await setDoc(
+        //         doc(db, "products", productId),
+        //         {
+        //             buyer_id: buyerId,
+        //             purchase_date: purchaseDate,
+        //             available: false,
+        //         },
+        //         {
+        //             merge: true,
+        //         }
+        //     );
+        // } else {
+        //     return NextResponse.json({ error: "Unathorized" }, { status: 401 });
+        // }
+
+        const buyerSubject = "Purchase Confirmation";
+        // const buyerText = `Thank you for your purchase of ${productName}.`;
+        const buyerText = `Thank you for your purchase of (product name).`;
+        // const buyerHtml = `<strong>Thank you for your purchase of ${productName}.</strong>`;
+        const buyerHtml = `<strong>Thank you for your purchase of (product name).</strong>`;
+
+        const sellerSubject = "Product Sold";
+        const sellerText = `Your product (product name) has been sold.`;
+        const sellerHtml = `<strong>Your product (product name) has been sold.</strong>`;
+
+        await Promise.all([
+            sendEmail({
+                to: "buczekzks@gmail.com",
+                subject: buyerSubject,
+                text: buyerText,
+                html: buyerHtml,
+            }),
+            sendEmail({
+                to: "hubertbukala@icloud.com",
+                subject: sellerSubject,
+                text: sellerText,
+                html: sellerHtml,
+            }),
+        ]);
 
         return NextResponse.json(
             { status: 200 },
