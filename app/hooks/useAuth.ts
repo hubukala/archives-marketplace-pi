435fda6365485bfcde71ba0c57c627ceaac69258
hubukala <hubertbukala@icloud.com>
2024-07-28 15:21:37 +0200

    authorization check hook added

---
+++ app/hooks/useAuth.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { auth } from "@/lib/firebase/config";
+import { onAuthStateChanged, User } from "firebase/auth";
+import axios from "axios";
+
+const useAuth = () => {
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                try {
+                    const token = await user.getIdToken();
+                    const response = await axios.get("/api/user/details", {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                            Accept: "application/json",
+                        },
+                    });
+                    setUser(response.data);
+                    setLoading(false);
+                } catch (error) {
+                    console.error("Error fetching user data:", error);
+                }
+            } else {
+                setLoading(false);
+                setUser(null);
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return { user, loading };
+};
+
+export default useAuth;
