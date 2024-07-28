e83f1cf24da1a03d8b0c3676a0baa5e14288d5c8
hubukala <hubertbukala@icloud.com>
2024-07-28 07:58:14 +0200

    fonts added

--- app/components/layout/home/style.tsx
+++ app/components/layout/home/style.tsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 import Image from "next/image";
+import { Archivo_Black } from "@next/font/google";
+
+const ArchivoBlack = Archivo_Black({
+    weight: "400",
+    subsets: ["latin"],
+});
 
 const HomeSection = styled.div`
     width: 100%;
@@ -12,4 +18,55 @@ const ImageFullScreen = styled(Image)`
     height: 100%;
 `;
 
-export { HomeSection, ImageFullScreen };
+const RecentlyPostedWrapper = styled.div`
+    padding-left: 1rem;
+    padding-right: 1rem;
+`;
+
+const BannerOne = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 28px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    border-radius: 20px;
+    height: 200px;
+    width: 500px;
+    padding: 20px;
+    background-color: #263d8b;
+    color: #e32226;
+    font-size: 36px;
+    font-family: ${ArchivoBlack.style.fontFamily};
+`;
+
+const BannerTwo = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 28px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    border-radius: 20px;
+    height: 200px;
+    width: 500px;
+    padding: 16px;
+    color: #263d8b;
+    border: 5px solid #e32226;
+    font-size: 36px;
+    font-family: ${ArchivoBlack.style.fontFamily};
+
+    &:hover {
+        color: #4869d8;
+        cursor: pointer;
+        border: 5px solid #fa6063;
+    }
+`;
+
+export {
+    HomeSection,
+    ImageFullScreen,
+    RecentlyPostedWrapper,
+    BannerOne,
+    BannerTwo,
+};
