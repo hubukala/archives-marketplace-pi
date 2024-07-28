8175717e6ffdbded539ebce05c3e6a8bf3cc4c83
hubukala <hubertbukala@icloud.com>
2024-07-28 15:20:09 +0200

    added mark as paid/mark as shipped features

--- app/(pages)/shop/[slug]/page.tsx
+++ app/(pages)/shop/[slug]/page.tsx
@@ -4,14 +4,15 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import useProduct from "@/lib/api/product";
 import {
     ButtonSection,
-    ButtonWrapper,
     DeleteModalButtonsWrapper,
     DeleteProductModalWrapper,
     GallerySection,
     ProductDescription,
-    ProductDetails,
+    ProductDetail,
     ProductPrice,
     ProductSection,
+    TrackingNumberButtonsWrapper,
+    TrackingNumberModalWrapper,
 } from "./_components/style";
 import ReactImageGallery from "react-image-gallery";
 import Button from "@/app/components/ui/button/button";
@@ -23,14 +24,27 @@ import Loader from "@/app/components/ui/loader/loader";
 import Modal from "@/app/components/ui/modal/modal";
 import { useState } from "react";
 import useProductDelete from "@/lib/api/product-delete";
+import { Input } from "@/app/components/ui/input/style";
+import * as Yup from "yup";
+import { Form, Formik } from "formik";
+import useProductUpdate from "@/lib/api/product-update";
 
-export default function Page() {
+export default function ProductDetails() {
     const {
         productDelete,
         loading: loadingDeleteProduct,
         error: errorDeleteProduct,
     } = useProductDelete();
+    const {
+        productUpdate,
+        loading: productUpdateLoading,
+        error: productUpdateError,
+    } = useProductUpdate();
+
     const [deleteProductModalOpen, setDeleteProductModalOpen] = useState(false);
+    const [trackingNumberModalOpen, setTrackingNumberModalOpen] =
+        useState(false);
+    const [markAsPaidModalOpen, setMarkAsPaidModalOpen] = useState(false);
 
     const pathname = usePathname();
     const router = useRouter();
@@ -79,6 +93,52 @@ export default function Page() {
         }
     };
 
+    const initialValues = {
+        trackingNumber: "",
+    };
+
+    const validationSchema = Yup.object({
+        trackingNumber: Yup.string().required(
+            "Please provide the tracking number"
+        ),
+    });
+
+    const onSubmit = async (values) => {
+        const payload = {
+            ...values,
+            id: product?.id,
+        };
+
+        const result = await productUpdate(payload);
+        if (result) {
+            notify({
+                type: "success",
+                message: "Tracking number added successfully",
+            });
+            setTrackingNumberModalOpen(false);
+        } else {
+            console.log("Failed to add the tracking number");
+        }
+    };
+
+    const markAsPaid = async () => {
+        const payload = {
+            paid: true,
+            id: product?.id,
+        };
+
+        const result = await productUpdate(payload);
+        if (result) {
+            notify({
+                type: "success",
+                message: "Product marked as paid",
+            });
+            setMarkAsPaidModalOpen(false);
+        } else {
+            console.log("Failed to mark item as paid");
+        }
+    };
+
     if (isLoading) {
         return (
             <ProductSection>
@@ -95,6 +155,16 @@ export default function Page() {
         );
     }
 
+    const showMarkAsPaidButton =
+        product?.available === false &&
+        product?.buyer_id === uid &&
+        !product?.paid;
+
+    const showMarkAsShippedButton =
+        product?.available === false &&
+        product?.user_id === uid &&
+        !product?.tracking_number;
+
     return (
         <div>
             <ProductSection>
@@ -102,14 +172,52 @@ export default function Page() {
                     <ReactImageGallery items={product?.image ?? []} />
                     <ProductDescription>
                         <h2>{product?.title}</h2>
-                        <ProductDetails>SIZE: {product?.size}</ProductDetails>
-                        <ProductDetails>
+                        <ProductDetail>SIZE: {product?.size}</ProductDetail>
+                        <ProductDetail>
                             CONDITION: {product?.condition}
-                        </ProductDetails>
+                        </ProductDetail>
                         {/* todo: add seller profile page */}
                         {/* <ProductDetails>SELLER: john</ProductDetails> */}
-                        <ProductDetails>{product?.description}</ProductDetails>
+                        <ProductDetail>{product?.description}</ProductDetail>
                         <ProductPrice>$ {product?.price ?? ""}</ProductPrice>
+                        {product?.buyer_id === uid &&
+                            product?.paid &&
+                            !product.tracking_number && (
+                                <>
+                                    <h3>
+                                        We&apos;ve send a notification to the
+                                        seller that you marked the item as paid
+                                    </h3>
+                                    <p>Your order will be shipped in 3 days.</p>
+                                </>
+                            )}
+                        {product?.buyer_id === uid &&
+                            product?.tracking_number && (
+                                <>
+                                    <h3>
+                                        Your order was marked as
+                                        &apos;SHIPPED&apos; by the seller
+                                    </h3>
+                                    <p>
+                                        {product?.shipping_carrier} tracking
+                                        number: {product?.tracking_number}
+                                    </p>
+                                </>
+                            )}
+                        {product?.user_id === uid &&
+                            product?.paid &&
+                            !product?.tracking_number && (
+                                <>
+                                    <h3>
+                                        Your listing was marked as
+                                        &apos;PAID&apos; by the buyer
+                                    </h3>
+                                    <p>
+                                        It is required to ship your item in 3
+                                        days since you receive your payment.
+                                    </p>
+                                </>
+                            )}
                     </ProductDescription>
                     <ButtonSection>
                         {product?.buyer_id ? (
@@ -120,7 +228,8 @@ export default function Page() {
                                     disabled={true}
                                 />
                             </>
-                        ) : product?.user_id === uid ? (
+                        ) : product?.user_id === uid &&
+                          product?.available === true ? (
                             <>
                                 <Button
                                     label="EDIT"
@@ -144,7 +253,43 @@ export default function Page() {
                                 />
                             </>
                         )}
+                        {showMarkAsShippedButton && (
+                            <>
+                                <Button
+                                    label="MARK AS SHIPPED"
+                                    variant="primary"
+                                    onClick={() =>
+                                        setTrackingNumberModalOpen(true)
+                                    }
+                                />
+                            </>
+                        )}
+                        {showMarkAsPaidButton && (
+                            <>
+                                <Button
+                                    label="MARK AS PAID"
+                                    variant="primary"
+                                    onClick={() => setMarkAsPaidModalOpen(true)}
+                                />
+                            </>
+                        )}
                     </ButtonSection>
+                    {product?.user_id === uid && (
+                        <p>
+                            You can always contact the buyer at:{" "}
+                            {product?.buyer_email}
+                        </p>
+                    )}
+                    {product?.buyer_id === uid && (
+                        <p>
+                            You can always contact the seller at:{" "}
+                            {product?.seller_email}
+                        </p>
+                    )}
+                    <p>
+                        or send a message to our support team at
+                        support@archives-marketplace.site
+                    </p>
                 </GallerySection>
             </ProductSection>
             <Modal open={deleteProductModalOpen} height="180px">
@@ -160,6 +305,57 @@ export default function Page() {
                             label="CLOSE"
                             variant="secondary"
                             onClick={() => setDeleteProductModalOpen(false)}
+                        />
+                    </DeleteModalButtonsWrapper>
+                </DeleteProductModalWrapper>
+            </Modal>
+            <Modal open={trackingNumberModalOpen} height="180px">
+                <TrackingNumberModalWrapper>
+                    <span>Enter your tracking number</span>
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={onSubmit}
+                    >
+                        <Form
+                            style={{ display: "flex", flexDirection: "column" }}
+                        >
+                            <Input
+                                type="text"
+                                name="trackingNumber"
+                                placeholder=""
+                            />
+                            <TrackingNumberButtonsWrapper>
+                                <Button
+                                    label="SUBMIT"
+                                    variant="primary"
+                                    type="submit"
+                                />
+                                <Button
+                                    label="CLOSE"
+                                    variant="secondary"
+                                    onClick={() =>
+                                        setTrackingNumberModalOpen(false)
+                                    }
+                                />
+                            </TrackingNumberButtonsWrapper>
+                        </Form>
+                    </Formik>
+                </TrackingNumberModalWrapper>
+            </Modal>
+            <Modal open={markAsPaidModalOpen} height="180px">
+                <DeleteProductModalWrapper>
+                    <p>Are you sure you want to mark your purchase as paid?</p>
+                    <DeleteModalButtonsWrapper>
+                        <Button
+                            label="MARK AS PAID"
+                            variant="primary"
+                            onClick={markAsPaid}
+                        />
+                        <Button
+                            label="CLOSE"
+                            variant="secondary"
+                            onClick={() => setMarkAsPaidModalOpen(false)}
                         />
                     </DeleteModalButtonsWrapper>
                 </DeleteProductModalWrapper>
