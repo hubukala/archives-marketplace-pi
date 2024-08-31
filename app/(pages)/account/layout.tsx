d46f2cd28a4469b269de7d200b721368e96d5347
hubukala <hubertbukala@icloud.com>
2024-08-31 17:03:47 +0200

    avatar upload added

--- app/(pages)/account/layout.tsx
+++ app/(pages)/account/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
     AccountWrapper,
     AccountInfoWrapper,
@@ -8,10 +8,14 @@ import {
     ContentWrapper,
     ContentBox,
 } from "./_components/style";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "@/lib/firebase/config";
 import SideBar from "@/app/components/layout/sidebar/sidebar";
 import { useRouter } from "next/navigation";
 import useAuth from "@/app/hooks/useAuth";
 import Loader from "@/app/components/ui/loader/loader";
+import { v4 as uuidv4 } from "uuid";
+import { useUserDetails } from "@/lib/api/user-details";
 
 export default function Account({
     children,
@@ -21,6 +25,11 @@ export default function Account({
     const router = useRouter();
     const [category, setCategory] = useState("/account");
     const { user, loading } = useAuth();
+    const fileInputRef = useRef(null);
+    const uniqueId = uuidv4();
+
+    const { userDetails, isLoading, isError, mutate, updater } =
+        useUserDetails();
 
     useEffect(() => {
         router.push(category);
@@ -45,6 +54,30 @@ export default function Account({
         },
     ];
 
+    const handleAvatarClick = () => {
+        fileInputRef?.current?.click();
+    };
+
+    const handleAvatarUpload = async (event) => {
+        const file = event?.target?.files[0];
+        const avatarRef = ref(storage, `avatars/${file.name + uniqueId}`);
+        const snapshot = await uploadBytes(avatarRef, file);
+        const url = await getDownloadURL(snapshot.ref);
+
+        const formData = {
+            avatar: url,
+        };
+
+        try {
+            await updater("/api/user/details", formData);
+            alert("User information updated successfully");
+            mutate(); // Re-fetch the user data to update the local state
+        } catch (err) {
+            console.error("Error updating user information:", err);
+            alert("Error updating user information");
+        }
+    };
+
     if (loading) {
         return (
             <AccountWrapper>
@@ -56,19 +89,22 @@ export default function Account({
     return (
         <AccountWrapper>
             <AccountInfoWrapper>
+                {category}
                 <FileUploadInput
                     type="file"
                     id="file"
                     name="file"
-                    // ref={fileInputRef}
-                    // onChange={(event) => handleAvatarUpload(event)}
+                    ref={fileInputRef}
+                    onChange={(event) => handleAvatarUpload(event)}
                 />
                 <Avatar
                     width={100}
                     height={100}
-                    src={"https://dummyimage.com/600x400/000/fff"}
+                    src={
+                        user?.avatar ?? "https://dummyimage.com/600x400/000/fff"
+                    }
                     alt="AV"
-                    // onClick={handleAvatarClick}
+                    onClick={handleAvatarClick}
                 />{" "}
                 <br />
                 {user?.fname} {user?.lname}
